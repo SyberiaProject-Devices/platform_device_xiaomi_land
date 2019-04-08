add93b82186d435cea45f788366f7577ad2c4953
weritos666 <weritos94@gmail.com>
2019-04-08 08:47:36 +0000

    Revert "land:Enable TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT,Set TARGET_PROVIDES_QTI_TELEPHONY_JAR, Fix Gcam"
    This reverts commit fdfdfe8da033af98b8d9f37143da6db9b85591f4.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,11 +212,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
 PROTOBUF_SUPPORTED := true
-#TARGET_RIL_VARIANT := caf
-TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
-
-# Telephony
-TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
+TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
